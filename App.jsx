a990933a6ee6486b490c70351d9ab89fceb66789
ÜMMÜGÜLSÜM SÖĞÜT <ummugulsum.sogut@ogr.gelisim.edu.tr>
2026-03-31 11:48:24 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,138 +1,63 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
 import './App.css'
 
-function ProfilBaslik({kullanici}){
+function Isim(){
+    return(
+<h1 className='selamlama'> Merhaba <span>Çisem Yaşar</span></h1>
+
+
+
+
+
+    );
+}
+
+const AltBilgi=()=>{
 return(
-    <div style={{padding:`20px`,
-        backgroundColor:`lightblue`,
-        color:`white`,
-        borderRadius:`12px`,
-        marginBottom:`20px`
-    }}>
-<h1>{kullanici.isim}</h1>
-<p>{kullanici.bio}</p>
+<footer  className='altbilgi'>
+    <p>2026- Ön Yüz Yazılım Geliştirme</p>
+</footer>
 
 
-
-    </div>
 
 );
 }
-function İstatistik({ etiket,deger}){
+
+function Sayac(){
+const[sayi,setSayi]=useState(0);
 return(
-<div style={{textAlign:`center`,
-    padding:`10px`}}>
-        <p><strong>{deger}</strong></p>
-        <p style></p>
+<div className='sayac-card'>
+    <p className='sayac-label'>Sayaç</p>
+    <h1 className={`sayac-deger ${sayi>0 ?  `pozitif` : sayi<0 ?`negatif`:0} `}>{sayi}</h1>
 
 
 
+<div className='butonlar'>   
+    <button className='btn artir' onClick={()=>setSayi(sayi+1)}>Artır</button>
+    <button className='btn artir' onClick={()=>setSayi(sayi-1)}>Azalt</button>
+    <button className='btn artir' onClick={()=>setSayi(0)}>Sıfır</button>
 
+</div>
+</div>
+);
+
+
+
+}
+
+
+function App(){
+return(
+<div className='app'>
+    <Isim></Isim>
+    <Sayac></Sayac>
+    <AltBilgi></AltBilgi>
 </div>
 
 
 );
 }
-
-
-
-function İstatikBari({tskipci,takip,gonderi}){
-return(
-    <div style={{ display:`flex`,
-justifyContent:`space-around`,
-border:`1 px soild gray`,
-borderRadius:`8px`,
-padding:`10px`,
-marginBottom:`20px`
-
-
-    }}>
-<İstatistik etiket="Gönderi" deger={gonderi}></İstatistik>
-<İstatistik etiket="Takipçi" deger={takipçi}></İstatistik>
-<İstatistik etiket="Takip" deger={takip}></İstatistik>
-
-
-
-
-
-    </div>
-);
-}
-
-function Gonderi({icerik,tarih,begeni}){
-return(
-<div style={{border:`1px soil gtay`,
-    borderRadius:`8px`,
-    padding:`16px`,
-    marginBottom:`10px`
-}}>
-    <p>{icerik}</p>
-    <p style={{color:`blue`}}>
-{tarih}-{begeni}begeni
-
-
-    </p>
-
-
-
-</div>
-
-
-
-
-
-);
-}
-
-function SosyalMedyaProfil(){
-const kullanici={
-    isim:"Efe Çetinkaya",
-    bio:"Yazılım öğrwncisi| React öğreniyorum",
-};
-
-const gonderiler=[
-    {id:1, 
-    icerik:"Bugün React bileşenlerini öğrendim!",
-tarih:"2 saat önce",
-begeni:12},
-
-{id:3,
-    icerik:"ilk projemi vite ile oluşturdum.",
-    tarih:"3 gün önce ",
-    begeni:24
-},
-];
-}
-
-return(
-<div style={{maxWidth:`500px`,
-    margin:`0 auto`,
-    padding:`20px`,
-}}>
-
-<ProfilBaslik kullanici={kullanici}></ProfilBaslik>
-<İstatikBari takipçi={1234}
-takip={567} gonderi={gonderiler.length}></İstatikBari>
-<h2>Göndereiler</h2>
-{gonderiler.map(g=>(
-    <Gonderi key={g.id}
-    icerik={g.icerik}
-    tarih={g.tarih}
-    begeni={g.begeni}></Gonderi>
-))}
-
-
-
-
-
-
-</div>
-
-
-
-
-
-
-)
+export default App;
